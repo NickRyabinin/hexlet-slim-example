d6d59f42bef5903f6b8a39971accec1f939c4f00
Николай Рябинин <rnn2002@mail.ru>
2023-08-18 20:27:34 +0300

    Modify src/Database.php

--- src/Database.php
+++ src/Database.php
@@ -22,7 +22,7 @@ class Database
     {
         return file_put_contents($this->path, base64_encode(json_encode($users, JSON_PRETTY_PRINT)));
     }
-    
+
     public function findUser(int $id): array|false
     {
         foreach ($this->users as $user) {
